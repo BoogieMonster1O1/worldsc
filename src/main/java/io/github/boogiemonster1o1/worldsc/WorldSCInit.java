9dc3271f19159d4289fed08868c192d9119c5617
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-06-06 21:56:29 +0530

    Initial Commit

---
+++ src/main/java/io/github/boogiemonster1o1/worldsc/WorldSCInit.java
@@ -0,0 +1,26 @@
+package io.github.boogiemonster1o1.worldsc;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class WorldSCInit implements ModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "worldsc";
+    public static final String MOD_NAME = "World Source Control";
+
+    @Override
+    public void onInitialize() {
+        log(Level.INFO, "Initializing");
+        //TODO: Initializer
+    }
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_NAME+"] " + message);
+    }
+
+}
